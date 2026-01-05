89ffe7337ea9d8e86cc700ce5bb9444e4ee4843d
Dhokiya Jenil <dhokiyajenil@gmail.com>
2026-01-05 11:09:34 +0530

    Completed Todo list adding function

--- Individual/to-do_list/src/CEA/todolist.jsx
+++ Individual/to-do_list/src/CEA/todolist.jsx
@@ -3,19 +3,47 @@ class ToDolist extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todo: [],
+      todos: [],
       inputValue: "",
     };
   }
+
+  handleInput = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  };
+  addTodo = () => {
+    if (this.state.inputValue.trim() == "") return;
+    const newtodo = {
+      id: Date.now(),
+      text: this.state.inputValue,
+      completed: false,
+    };
+    this.setState((prevState) => ({
+      todos: [...prevState.todos, newtodo],
+      inputValue: "",
+    }));
+    console.log(this.state.todos);
+  };
 
   render() {
     const { todos, inputValue } = this.state;
     return (
       <>
-        <input type="text" value={inputValue} />
-        <button>Add</button>
+        <input type="text" value={inputValue} onChange={this.handleInput} />
         <br />
-        <ul>{}</ul>
+        <button onClick={this.addTodo}>Add</button>
+        <br />
+        <ul>
+          {todos.map((x) => (
+            <li key={x.id}>
+              <input type="checkbox" value={x.completed} />
+              {x.text}
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
       </>
     );
   }
