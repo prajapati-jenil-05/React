48f39642eb3504192cb4b55b3e0b3333bea01175
Dhokiya Jenil <dhokiyajenil@gmail.com>
2026-01-09 09:00:19 +0530

    Refactored code

--- react_arzu/api/index.js
+++ react_arzu/api/index.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.all("/", (req, res) => res.send("from all"));
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/food{s}", (req, res) => res.send("food or foods"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- react_arzu/api_testing/index.js
+++ react_arzu/api_testing/index.js
@@ -1,30 +1,32 @@
-const e = require('express')
-const express = require('express')
-const app = express()
-const port = 3000
+const e = require("express");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/name', (req, res) => res.send('Welcome Kaju!'))
-app.get('/student', (req, res) => res.json({"name":"Durgesh"}))
-app.get('/uni', (req, res) => res.send('RKU'))
-app.get('/flf', (req, res) => {
-    const firstname = req.query.firstname;
-    const lastname = req.query.lastname;
-    res.send(`Full name is: ${firstname} ${lastname}`);
-})
-app.get('/myname/:fname/:lname',(req,res)=>{
-    var firstname=req.params.fname;
-    var lastname=req.params.lname;
-    res.send(`My full name is: ${firstname} ${lastname}`);
-})
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/name", (req, res) => res.send("Welcome Kaju!"));
+app.get("/student", (req, res) => res.json({ name: "Durgesh" }));
+app.get("/uni", (req, res) => res.send("RKU"));
+app.get("/flf", (req, res) => {
+  const firstname = req.query.firstname;
+  const lastname = req.query.lastname;
+  res.send(`Full name is: ${firstname} ${lastname}`);
+});
+app.get("/myname/:fname/:lname", (req, res) => {
+  var firstname = req.params.fname;
+  var lastname = req.params.lname;
+  res.send(`My full name is: ${firstname} ${lastname}`);
+});
 
-app.get('/arithmetic/:num1/:num2',(req,res)=>{
-    var number1=parseInt(req.params.num1);
-    var number2=parseInt(req.params.num2);
-    var sum=number1+number2;
-    var difference=number1-number2;
-    var product=number1*number2;
-    var division=number1/number2;
-    res.send(`Addition: ${sum} \n Subtraction: ${difference} \n Multiplication: ${product} \n Division: ${division}`);
-})
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/arithmetic/:num1/:num2", (req, res) => {
+  var number1 = parseInt(req.params.num1);
+  var number2 = parseInt(req.params.num2);
+  var sum = number1 + number2;
+  var difference = number1 - number2;
+  var product = number1 * number2;
+  var division = number1 / number2;
+  res.send(
+    `Addition: ${sum} \n Subtraction: ${difference} \n Multiplication: ${product} \n Division: ${division}`
+  );
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
